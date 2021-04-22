84e253e35b66b7f9aa60250a8826104f16f7a7fb
Marcela Silva <marcela.ops@hotmail.br>
2021-04-22 18:22:40 -0300

    exercicio 1 de operações aritiméticas e sem querer foi o scripts.js que foram os exercícios desenvolvidos ao longo do conteúdo.

---
+++ modulo_1/bloco_4/dia_1/scripts.js
@@ -0,0 +1,39 @@
+//Parte II
+const name = "Carolina";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1987;
+birthYear = 2030;
+birthCity = 'São Paulo';
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+
+//Parte III
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+console.log(typeof patientAge) //aparece como undefined, pois n declaramos essa variável.
+
+
+
+let base = 5;
+let altura = 8;
+
+let area = (base*altura);
+console.log(area);
+
+let perimetro = 2*base+2*altura;
+console.log(perimetro);
+
+// Parte IV
