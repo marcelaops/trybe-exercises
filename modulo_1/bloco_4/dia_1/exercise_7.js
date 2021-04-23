3a3a0a29e8a3c6bc64c0649df5803535ea522fc2
Marcela Silva <marcela.ops@hotmail.br>
2021-04-22 22:01:08 -0300

    refazendo exercício 7

--- modulo_1/bloco_4/dia_1/exercise_7.js
+++ modulo_1/bloco_4/dia_1/exercise_7.js
@@ -1,22 +1,22 @@
 let nota = 101;
-if (nota<= 100 && nota>=90){
+if(nota<0 || nota >10){
+    console.log("Erro, nota inválida");
+}
+else if (nota>=90){
     console.log("A");
 }
-else if (nota<90 && nota>=80){
+else if (nota>=80){
     console.log("B");
 }
-else if(nota<80 && nota>=70){
+else if(nota>=70){
     console.log("C");
 }
-else if(nota<70 && nota>=60){
+else if(nota>=60){
     console.log("D");
 }
-else if(nota<60 && nota>=50){
+else if(nota>=50){
     console.log("E");
 }
-else if(nota<50){
-    console.log("F");
-}
 else{
-    console.log("Erro, nota inválida");
+    console.log("F");
 }
