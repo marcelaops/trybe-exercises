227a77a42640bbf3a100bdfe8f962969e7bebe4b
Marcela Silva <marcela.ops@hotmail.br>
2021-04-22 21:45:47 -0300

    Exercício 7 - converter nota

---
+++ modulo_1/bloco_4/dia_1/exercise_7.js
@@ -0,0 +1,22 @@
+let nota = 101;
+if (nota<= 100 && nota>=90){
+    console.log("A");
+}
+else if (nota<90 && nota>=80){
+    console.log("B");
+}
+else if(nota<80 && nota>=70){
+    console.log("C");
+}
+else if(nota<70 && nota>=60){
+    console.log("D");
+}
+else if(nota<60 && nota>=50){
+    console.log("E");
+}
+else if(nota<50){
+    console.log("F");
+}
+else{
+    console.log("Erro, nota inválida");
+}
