6c8d8c96caa12a821949566329571b9e3a59ecd2
Marcela Silva <marcela.ops@hotmail.br>
2021-04-28 09:47:09 -0300

    exercícios de fixação de for

--- modulo_1/bloco_4/dia_2/exerciciosDeFixacao.js
+++ modulo_1/bloco_4/dia_2/exerciciosDeFixacao.js
@@ -1,3 +1,4 @@
+// Exercícios de fixação de Arrays
 // Exercício 1 - Obtenha o valor "Serviços" do array menu :
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
@@ -13,3 +14,13 @@ console.log(indexOfPortfolio);
 menu.push('Contato');
 
 console.log(menu);
+
+
+// Exercícios de fixação de Fors
+// Exercício 1 - Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index+=1)
+{
+    console.log(groceryList[index]);
+}
