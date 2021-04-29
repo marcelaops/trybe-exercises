2b16559db4c1e093a5aa5905201500e20bcf75f8
Marcela Silva <marcela.ops@hotmail.br>
2021-04-29 16:06:34 -0300

    Conteúdo Parte II praticando getElementById

---
+++ modulo_1/bloco_5/dia_1/practiceExercises.js
@@ -0,0 +1,6 @@
+// console.log(document.getElementById("page-title").innerText);
+
+console.log(document.getElementById("second-paragraph").innerText = "Segundo parágrafo alterado");
+
+console.log(document.getElementById("subtitle").innerText = "Subtítulo alterado");
+
