31f0684f7e414b74eb913ff9cefc0b771c02f140
Marcela Silva <marcela.ops@hotmail.br>
2021-04-29 18:23:08 -0300

    getElementsByClassName e getElementsByTagName

--- modulo_1/bloco_5/dia_1/practiceExercises.js
+++ modulo_1/bloco_5/dia_1/practiceExercises.js
@@ -1,6 +1,11 @@
 // console.log(document.getElementById("page-title").innerText);
 
-console.log(document.getElementById("second-paragraph").innerText = "Segundo parágrafo alterado");
+// console.log(document.getElementById("second-paragraph").innerText = "Segundo parágrafo alterado");
 
-console.log(document.getElementById("subtitle").innerText = "Subtítulo alterado");
+// console.log(document.getElementById("subtitle").innerText = "Subtítulo alterado");
+
+console.log(document.getElementsByClassName('paragraph'));
+console.log(document.getElementsByClassName('paragraph')[0].style.border = 'solid');
+console.log(document.getElementsByTagName('h4')[0].style.color = 'red');
+
 
