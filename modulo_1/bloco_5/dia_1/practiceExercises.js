6f70a8bf437d60571e2b4b62dd660ec6f5263bb2
Marcela Silva <marcela.ops@hotmail.br>
2021-04-29 18:54:10 -0300

    praticando querySelector e query SelectorAll

--- modulo_1/bloco_5/dia_1/practiceExercises.js
+++ modulo_1/bloco_5/dia_1/practiceExercises.js
@@ -4,8 +4,10 @@
 
 // console.log(document.getElementById("subtitle").innerText = "Subtítulo alterado");
 
-console.log(document.getElementsByClassName('paragraph'));
-console.log(document.getElementsByClassName('paragraph')[0].style.border = 'solid');
-console.log(document.getElementsByTagName('h4')[0].style.color = 'red');
+// console.log(document.getElementsByClassName('paragraph'));
+// console.log(document.getElementsByClassName('paragraph')[0].style.border = 'solid');
+// console.log(document.getElementsByTagName('h4')[0].style.color = 'red');
 
-
+console.log(document.querySelectorAll('paragraph'));
+console.log(document.querySelector('.paragraph').style.background = 'red');
+console.log(document.querySelectorAll('.paragraph')[1].style.background = 'red');
