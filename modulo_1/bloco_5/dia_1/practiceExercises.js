8f73bfa8849fbcca5cf079b9ed4d84cca13798f4
Marcela Silva <marcela.ops@hotmail.br>
2021-04-29 19:29:07 -0300

    P3 começei a pratica o getElementBy e querySelector

--- modulo_1/bloco_5/dia_1/practiceExercises.js
+++ modulo_1/bloco_5/dia_1/practiceExercises.js
@@ -8,6 +8,14 @@
 // console.log(document.getElementsByClassName('paragraph')[0].style.border = 'solid');
 // console.log(document.getElementsByTagName('h4')[0].style.color = 'red');
 
-console.log(document.querySelectorAll('paragraph'));
-console.log(document.querySelector('.paragraph').style.background = 'red');
-console.log(document.querySelectorAll('.paragraph')[1].style.background = 'red');
+// console.log(document.querySelectorAll('paragraph'));
+// console.log(document.querySelector('.paragraph').style.background = 'red');
+// console.log(document.querySelectorAll('.paragraph')[1].style.background = 'red');
+
+// Parte III - Qual seletor usar?
+
+console.log(document.getElementById('header-container').style.backgroundColor ='green');
+
+// Nâo está dando certo esse aí 
+// console.log(document.getElementsByClassName("emergency-tasks").style.backgroundColor = 'pink');
+
