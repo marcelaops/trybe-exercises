e6890fabf9867ee3116d4539ade38d2a066ec427
Marcela Silva <marcela.ops@hotmail.br>
2021-05-02 12:01:47 -0300

    exercícios de fixação de for/in

--- modulo_1/bloco_4/dia_4/exerciciosDeFixacao.js
+++ modulo_1/bloco_4/dia_4/exerciciosDeFixacao.js
@@ -1,26 +1,48 @@
+//OBJETOS
 //Exercíio 1
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3
-    },
-     //Exercício 3
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3
+//     },
+//      //Exercício 3
+//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 
+// };
+
+// // console.table(player);
+
+// //Exercícios 2
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+// //Exercício 4
+// let quantasVezesMelhorDoMundo = player.bestInTheWorld.length;
+// console.log('A jogadora Marta Silva foi eleita a melhor do mundo por ' + quantasVezesMelhorDoMundo + ' vezes');
+
+// //Exercício 5 
+// console.log('A jogadora possui ' + player.medals.golden +' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
+
+//FOR/IN
+
+// Exercício 1
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+for (let key in names) {
+    console.log('Olá ' + names[key]);
+}
+
+//Exercício 2
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
 };
-
-// console.table(player);
-
-//Exercícios 2
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
-
-//Exercício 4
-let quantasVezesMelhorDoMundo = player.bestInTheWorld.length;
-console.log('A jogadora Marta Silva foi eleita a melhor do mundo por ' + quantasVezesMelhorDoMundo + ' vezes');
-
-//Exercício 5 
-console.log('A jogadora possui ' + player.medals.golden +' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
-
+for (let key in car) {
+    console.log(key + ': ' +car[key]);
+}
