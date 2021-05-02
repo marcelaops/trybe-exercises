b6fe39eea0ea48871482b4b09943ea708ab702c2
Marcela Silva <marcela.ops@hotmail.br>
2021-05-02 13:15:27 -0300

    P1e4 for/in mostrando os valores das chaves

--- modulo_1/bloco_4/dia_4/exercises.js
+++ modulo_1/bloco_4/dia_4/exercises.js
@@ -12,6 +12,7 @@ let info = {
 
 //Exercício 2
 // console.log(info);
+//ou colocar aqui em baixo info.reccorente = 'Sim'; ou info['recorrente'] = 'Sim';
 
 //Exercício 3
 // for (let key in info) {
@@ -19,6 +20,23 @@ let info = {
 // }
 
 //Exercício 4
-for (let key in info) {
-    console.log (info[key]);
+// for (let key in info) {
+//     console.log (info[key]);
+// }
+
+//Exercícios 5
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    //Exercício 2
+    recorrente: 'Sim'
+} 
+
+for (let key in info){
+    if (info[key] === info.recorrente && info[key] === 'Sim' && info2[key] === 'Sim' ) {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key] + ' e ' + info2[key]);
+    }
 }
