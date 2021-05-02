aefaf94f3a518e8d8bf4acaef36bea31aea74e78
Marcela Silva <marcela.ops@hotmail.br>
2021-05-02 12:52:45 -0300

    Parte 1 exer 1 - imprimindo mensagem

---
+++ modulo_1/bloco_4/dia_4/exercises.js
@@ -0,0 +1,10 @@
+//PARTE I - OBJETOS E FOR/IN
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+ 
+//Exercício 1
+console.log('Bem vinda, ' + info.personagem);
+  
