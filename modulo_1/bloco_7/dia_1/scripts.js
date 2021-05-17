b395b5622d4cf528eb92c7cc4180cf39d2039980
Marcela Silva <marcela.ops@hotmail.br>
2021-05-17 18:36:24 -0300

    PIex1 modificando var e a função p arrow function

--- modulo_1/bloco_7/dia_1/scripts.js
+++ modulo_1/bloco_7/dia_1/scripts.js
@@ -1,16 +1,35 @@
 // PARTE I
 
 //Exercício 1
-function testingScope(escopo) {
+//função original: 
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// testingScope(true);
+
+// Trocando var  ifScope por let, pois a variável vai ser reatribuida depois. E trocando var elseScope por const. 
+//Também udando p arrow function
+
+const testingScope = (escopo) => {
     if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
     }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+}
 
-  testingScope(true);
+testingScope(false);
+
+
