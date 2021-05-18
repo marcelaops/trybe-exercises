cb507195769f7d5a4e46b5a0429a78b017d11caf
Marcela Silva <marcela.ops@hotmail.br>
2021-05-17 21:12:10 -0300

    p2ex1 comecei a fazer

---
+++ modulo_1/bloco_7/dia_1/parte2.js
@@ -0,0 +1,8 @@
+//Exercício 1 - Crie uma função que receba um número e retorne seu fatorial.
+
+const fatorial = (numero) => {
+    for (let index = numero; index > numero; index-=1) {
+        let fatorial = 
+    }
+}
+
