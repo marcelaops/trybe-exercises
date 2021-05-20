e4d4a3d1dd60f33e77c80d2e9abf20ac8e1d94c3
Marcela Silva <marcela.ops@hotmail.br>
2021-05-20 09:52:21 -0300

    p2exer6 total estudantes

--- modulo_1/bloco_7/dia_2/parte2.js
+++ modulo_1/bloco_7/dia_2/parte2.js
@@ -23,23 +23,40 @@ const lesson3 = {
 const addToLesson2 = (object, turno, value) => object.turno = value;    //ou poderia ter feito object[turno] = value
 
 addToLesson2(lesson2, 'turno', 'manhã');
-console.log(lesson2); 
+// console.log(lesson2); 
 
 //2.Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
   
 const listKeys = (object) => Object.keys(object);
-console.log(listKeys(lesson2)); 
+// console.log(listKeys(lesson2)); 
 
 //3.Crie uma função para mostrar o tamanho de um objeto.
 
 const sizeOfObj = (object) => listKeys(object).length;   
-console.log(sizeOfObj(lesson1));
+// console.log(sizeOfObj(lesson1));
 
 //4.Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
 const listValues = (object) => Object.values(object);
-console.log(listValues(lesson1));
+// console.log(listValues(lesson1));
 
 //5.Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});  //estava wquebrando a cabeça aqui, vi q faltava as {} p os sources tb (nesse casso as lessons).
+// console.log(allLessons);
 
 //6.Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+const totalStudents = (object) => {
+  let total = 0;
+  const students = Object.keys(object);   //tava quebrando a cabeça aqui tb pq estava achando q deveria usar o Object.value
+  for (index in students) {
+    total += object[students[index]].numeroEstudantes;
+  }
+  return total;
+}
+// console.log(totalStudents(allLessons));
+
+//7.Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+
+
+
+//8.Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave
