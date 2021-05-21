3f4ba36b8e3171440769c53a162c0987d8ecb154
Marcela Silva <marcela.ops@hotmail.br>
2021-05-21 20:21:30 -0300

    parte I exe2 verificações função myRemove

--- modulo_1/bloco_7/dia_3/p1ex2.js
+++ modulo_1/bloco_7/dia_3/p1ex2.js
@@ -1,3 +1,5 @@
+//Parte I exercício 2
+
 const assert = require('assert');
 
 function myRemove(arr, item) {
@@ -9,15 +11,22 @@ function myRemove(arr, item) {
   }
   return newArr;
 }
-// console.log(myRemove([1, 2, 3], 4));
-
-// implemente seus testes aqui
-
-//1.Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-// assert.strictEqual
 
 
+// primeiro checando se é uma função
+assert.strictEqual(typeof myRemove, 'function');
 
-//2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-//3. Verifique se o array passado por parâmetro não sofreu alterações
-//4. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+//2.1.Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+//deepStricEqual p array e objetos
+
+//2.2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+//2.3. Verifique se o array passado por parâmetro não sofreu alterações
+myRemove([4, 3, 2, 1], 3);
+assert.deepStrictEqual(myRemove([4, 3, 2, 1], 3), [4, 2, 1]);
+
+//2.4. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+console.log(myRemove([1, 2, 3, 4], 5));
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
