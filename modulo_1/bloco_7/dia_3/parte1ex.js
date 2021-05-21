fc6ccf62555a94ef48504d8f13cdf4cd48151056
Marcela Silva <marcela.ops@hotmail.br>
2021-05-21 19:07:35 -0300

    pIexe1 implementando testes na função soma

---
+++ modulo_1/bloco_7/dia_3/parte1ex.js
@@ -0,0 +1,35 @@
+//Parte 1
+//1. A função sum(a, b) retorna a soma do parâmetro a com o b
+
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+//Eu n tinha fetio, mas depois de completar a resposta vi q temos que checar se a suposta função é mesmo uma função, antes de checar se ela funciona.
+assert.strictEqual(typeof sum, 'function');
+
+// Teste se o retorno de sum(4, 5) é 9
+const expected = sum(4, 5);
+assert.strictEqual(expected, 9, '4 + 5 = 9');
+
+// Teste se o retorno de sum(0, 0) é 0
+const expected2 = sum(0, 0);
+assert.strictEqual(expected2, 0, '0 + 0 = 0');
+
+// Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+
+assert.throws(() => { 
+    sum(4, '5'); 
+});
+
+//Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.throws(() => {
+    sum(4, '5');
+}, 'Error: parameters must be numbers');
