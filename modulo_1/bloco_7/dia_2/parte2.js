f5e3464f35cb66bd66c4fdb7165cc4acddcaa92b
Marcela Silva <marcela.ops@hotmail.br>
2021-05-20 22:17:44 -0300

    tentando exer8 parte II

--- modulo_1/bloco_7/dia_2/parte2.js
+++ modulo_1/bloco_7/dia_2/parte2.js
@@ -42,21 +42,12 @@ const listValues = (object) => Object.values(object);
 
 //5.Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3
 
-const allLessons = Object.assign({}, lesson1, lesson2, lesson3);  //estava quebrando a cabeça aqui, vi q faltava as {} p os sources tb (nesse casso as lessons).
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});  //as {}'s vão pq ele que colocar os 3 objetos, como objetos do allLessons, se n colocar ela as keys iguais se sobreescreverm e vale o último valor passado.
+
 // console.log(allLessons);
 
 
 //6.Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
-
-
-
-
-
-
-
-
-
-
 
 const totalStudents = (object) => {
     let total = 0;
@@ -66,7 +57,7 @@ const totalStudents = (object) => {
     }
     return total;
   }
-//console.log(totalStudents(allLessons));
+// console.log(totalStudents(allLessons));
  
 
 //7.Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
@@ -75,7 +66,32 @@ const getValueByNumber = (object, position) => Object.values(object)[position];
 
 // console.log(getValueByNumber(lesson1, 0));
 
+
 //8.Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave
+
+
+
+const verifyPair = (object, key, value) => {
+  const properties = [key, value];
+  
+  for (index in object){
+    if (Object.entries(object)[index] === properties) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+
+
+
+
+
+
+
+
+
 
 // const verifyPair = (object, key, value) => {
 //   if (Object.keys(object) === key && Object.values(object)[key] === value) {
