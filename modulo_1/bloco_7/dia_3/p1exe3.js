0cec310408e0a6fe1ee8ec559e255ca84aad9124
Marcela Silva <marcela.ops@hotmail.br>
2021-05-21 21:07:37 -0300

    parI exe3 verificações função myRemoveWithoutCopy

--- modulo_1/bloco_7/dia_3/p1exe3.js
+++ modulo_1/bloco_7/dia_3/p1exe3.js
@@ -1,9 +1,12 @@
+// 3. A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
+
+
 const assert = require('assert');
 
 function myRemoveWithoutCopy(arr, item) {
   for (let index = 0, len = arr.length; index < len; index += 1) {
     if (arr[index] === item) {
-      arr.splice(index, 1);
+      arr.splice(index, 1);   //shttps://www.w3schools.com/jsref/jsref_splice.asp
       index -= 1;
       len -= 1;
     }
@@ -12,4 +15,17 @@ function myRemoveWithoutCopy(arr, item) {
   return arr;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(typeof myRemoveWithoutCopy, 'function');
+
+//3.1 Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+
+//3.2 Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+//3.3 Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+console.log(myRemoveWithoutCopy([4, 3, 2, 1], 3));
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+
+//3.4 Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
