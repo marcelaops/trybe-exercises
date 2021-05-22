cb94c3003e264c3e909d838faf2cf2e2d7e22d58
Marcela Silva <marcela.ops@hotmail.br>
2021-05-22 13:49:21 -0300

    p2ex2 função wordLengths

--- modulo_1/bloco_7/dia_3/p2ex2.js
+++ modulo_1/bloco_7/dia_3/p2ex2.js
@@ -1,7 +1,13 @@
 //2.Escreva a função wordLengths para passar nos testes já implementados.
 
 const assert = require('assert');
-// escreva a função wordLengths aqui
+const wordLengths = (arrayOfWords) => {
+    let arrayLength = [];
+    for (let index = 0; index < arrayOfWords.length; index += 1){
+        arrayLength.push(arrayOfWords[index].length);
+    } 
+    return arrayLength;
+}
 
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
