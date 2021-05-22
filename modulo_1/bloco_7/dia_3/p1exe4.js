9e74d4ec8b0e87c8a5746bb159ee58d8a700b64d
Marcela Silva <marcela.ops@hotmail.br>
2021-05-21 21:16:30 -0300

    parI exe4 verificações função myFizzBuz

--- modulo_1/bloco_7/dia_3/p1exe4.js
+++ modulo_1/bloco_7/dia_3/p1exe4.js
@@ -1,4 +1,4 @@
-//A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
+//4. A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
 
 const assert = require('assert');
 
@@ -10,10 +10,19 @@ function myFizzBuzz(num) {
   return num;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(typeof myFizzBuzz, 'function');
 
-//1.Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
-//2.Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
-//3.Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
-//4.Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
-//5.Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+//4.1.Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+
+//4.2.Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(3), 'fizz');
+
+//4.3.Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(5), 'buzz');
+
+//4.4.Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+assert.notDeepStrictEqual(myFizzBuzz(1), '');
+
+//4.5.Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz('oi'), false);
