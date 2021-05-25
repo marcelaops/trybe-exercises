36e1efee82cacf360a9a4b7620ba868df22b3d18
Marcela Silva <marcela.ops@hotmail.br>
2021-05-25 09:08:49 -0300

    arrumando diretório 2

--- modulo_1/bloco_8/dia_1/exercicio3.js
+++ modulo_1/bloco_8/dia_1/exercicio3.js
@@ -1,4 +1,3 @@
-// 3. Crie uma HOF utilizando o conceito de currying . Passe como parâmetros a resposta correta (Ex: Gabarito) para uma pergunta e uma resposta a ser validada (Ex: Resposta de uma pessoa). Sua HOF deve checar se a resposta enviada pela pessoa usuária é correta. Se for, a função retorna true , se não for, a função retorna false .
-// A validação da resposta não deve ser case sensitive.
-// Deve ser possível chamar a HOF criada de forma encadeada (Ex: checarResposta('Gabarito')('Teste'))
+// 3. Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
 
