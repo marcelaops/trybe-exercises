7674b6854a354b750693401d45026a7a0c4dce04
Marcela Silva <marcela.ops@hotmail.br>
2021-05-26 15:58:35 -0300

    exe7 function authorWith3DotsOnName

--- modulo_1/bloco_8/dia_3/exercise7.js
+++ modulo_1/bloco_8/dia_3/exercise7.js
@@ -70,6 +70,11 @@ const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
   // escreva seu código aqui
+  return books.find((book) => (book.author.name
+    .split(' ')
+    .filter((word) => word
+    .endsWith('.')).length === 3))
+    .name;
 }
-
+// console.log(authorWith3DotsOnName());
 assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
