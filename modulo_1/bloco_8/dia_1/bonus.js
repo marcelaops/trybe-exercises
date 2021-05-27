5d5c41720a720d2028803996be72ee9d4199f16b
Marcela Silva <marcela.ops@hotmail.br>
2021-05-27 09:24:08 -0300

    exe8.1 bônus funcão dano dragão

---
+++ modulo_1/bloco_8/dia_1/bonus.js
@@ -0,0 +1,35 @@
+// Parte I - Game Actions Simulator
+
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// 1 - Crie uma função que retorna o dano do dragão.
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
+const dano = (dragon) => {
+    return Math.floor(Math.random() * (dragon.strength - 15) + 15);
+}; 
+// console.log(dano(dragon));
+
+// 2 - Crie uma função que retorna o dano causado pelo warrior .
+// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
