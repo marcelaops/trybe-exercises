4d9f2f7d9c44ca9b74c9cb2f60fbc0c114564119
Marcela Silva <marcela.ops@hotmail.br>
2021-05-31 11:10:40 -0300

    exer4 object destructuring

--- modulo_1/bloco_8/dia_5/exercise4.js
+++ modulo_1/bloco_8/dia_5/exercise4.js
@@ -32,7 +32,14 @@ const people = [
 
 // escreva filterPeople abaixo
 
-const filteredPeople = filterPeople(people);
+const filterPeople = (person) => { 
+  return person.filter(({ bornIn, nationality }) => (bornIn >= 1901 && bornIn <= 2000 && nationality === 'Australian'));
+}
+const filteredPeople = filterPeople(people);  
+
+// nesse exercícios estava tentando usar o map antes, dai depois de olhar o gabarito percebi que o filter cabia melhor.
+
+console.log(filteredPeople);
 
 assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
 assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
