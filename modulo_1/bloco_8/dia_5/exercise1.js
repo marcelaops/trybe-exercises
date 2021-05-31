30da708a9a7ff007c7a1072192180def9b79975d
Marcela Silva <marcela.ops@hotmail.br>
2021-05-31 09:47:32 -0300

    exercise 1 - spread operator

--- modulo_1/bloco_8/dia_5/exercise1.js
+++ modulo_1/bloco_8/dia_5/exercise1.js
@@ -1,4 +1,4 @@
-// 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .Dica: use spread operator .
+// 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54. Dica: use spread operator .
 
 const assert = require('assert');
 
@@ -10,5 +10,12 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+  assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]); 
+  // altere a chamada da funcao rectangleArea
+  console.log(rectangle[0] * rectangle[1]);
 });
+
+
+// console.log(rectangleArea(...rectangle1));
+// console.log(rectangleArea(...rectangle2));
+// console.log(rectangleArea(...rectangle3));
