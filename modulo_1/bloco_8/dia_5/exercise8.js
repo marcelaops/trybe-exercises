bc7b18afe86aaa42e8ddcf50df437078ac3f087d
Marcela Silva <marcela.ops@hotmail.br>
2021-05-31 11:55:37 -0300

    exer8

--- modulo_1/bloco_8/dia_5/exercise8.js
+++ modulo_1/bloco_8/dia_5/exercise8.js
@@ -4,6 +4,14 @@
 const assert = require('assert');
 
 // escreva greet abaixo
+const greet = (name, greeting) => typeof greeting === 'undefined' ? (`Hi ${name}`) : `${greeting} ${name}` ;
+
+// uma maneira mais concisa (gabarito):
+const greet = (name, msg = 'Hi') => `${msg} ${name}`;
+
+console.log(greet('John'));
+console.log(greet('John', 'Good morning'));
+
 
 assert.strictEqual(greet('John'), 'Hi John');
 assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
