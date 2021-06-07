be8b37b2a147ff0174614b3f2f74ab7b262c4743
Marcela Silva <marcela.ops@hotmail.br>
2021-06-07 19:26:15 -0300

    exercise 1

--- modulo_1/bloco_9/apiScript.js
+++ modulo_1/bloco_9/apiScript.js
@@ -1,8 +1,33 @@
-// apiScript.js
+// Esse exercício ele foi fazendo passo á passo e eu fui trocando o corpo do código conforme o enunciado ia acrescentndo coisas. 
+
+// eu tava fazendo assim, uma func appendJoke separado, mas como fiquei perdida e fui olhar o gabarito, vi que ele estava fazendo dentro do fetch mesmo, copiei o q tinha no corpo dessa func no fetch e deu certo. Mas ficou muito mais extenso do q no gabarito.
+
+// const appendJoke = (joke) => {
+//     const jokeContainer = document.querySelector('#jokeContainer');
+//     const p = document.createElement('p');
+//     p.innerHTML = data.joke;
+//     jokeContainer.appendChild(p);
+// };
+
+// apiScript.js  
+
+
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => {
+        const jokeContainer = document.querySelector('#jokeContainer');
+        const p = document.createElement('p');
+        p.innerHTML = data.joke;
+        jokeContainer.appendChild(p);
+    });
 };
 
 window.onload = () => fetchJoke();
