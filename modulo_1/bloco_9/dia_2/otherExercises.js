603d3bd1b3b4a2779c41b016455961c4e2aa5d2b
Marcela Silva <marcela.ops@hotmail.br>
2021-06-08 15:04:13 -0300

    refatorando exercise 2

--- modulo_1/bloco_9/dia_2/otherExercises.js
+++ modulo_1/bloco_9/dia_2/otherExercises.js
@@ -1,20 +1,22 @@
 // 2
 const promise = new Promise((resolve, reject) => {
     const arrayNumber = [];
-    for (let index = 0; index < 9; index += 1) {
+    for (let index = 0; index < 10; index += 1) {
         const number = Math.floor(Math.random() * 51);
         arrayNumber.push(number);
     }
     const square = arrayNumber.map((number) => number * number);
+		console.log(square);
     const sumArray = square.reduce((acc, curr) => acc + curr);
     console.log(sumArray);
     if (sumArray > 8000) {
         return reject();
     }
     resolve();
-})
-.then(() => console.log('Promise resolvida'))
-.catch(() => console.log('Promise rejeitada'));
+});
+promise
+	.then(() => console.log('Promise resolvida'))
+	.catch(() => console.log('Promise rejeitada'));
 
 
 
