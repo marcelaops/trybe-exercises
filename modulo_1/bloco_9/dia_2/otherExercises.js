97c1a91870d0145320d1dd1d37b4b1672c6e824a
Marcela Silva <marcela.ops@hotmail.br>
2021-06-08 19:16:55 -0300

    exercise 4 final

--- modulo_1/bloco_9/dia_2/otherExercises.js
+++ modulo_1/bloco_9/dia_2/otherExercises.js
@@ -48,23 +48,24 @@ fetchPromise()
 
 // 4. Quando a Promise for rejeitada, imprima, via console.log , a frase "É mais de oito mil! Essa promise deve estar quebrada!"
 const fetchPromise = () => {
-  const myPromise = new Promise((resolve, reject) => {
-    const myArray = Array.from(
-      { length: 10 },
-      () =>  Math.floor(Math.random() * 50) + 1
-    );
-    const sum = myArray.map(number => number * number)
-                       .reduce((number, acc) => number + acc, 0);
+	const promise = new Promise((resolve, reject) => {
+		const arrayNumber = [];
+		for (let index = 0; index < 10; index += 1) {
+				const number = Math.floor(Math.random() * 51);
+				arrayNumber.push(number);
+		}
+		const square = arrayNumber.map((number) => number * number);
+		console.log(square);
+		const sumArray = square.reduce((acc, curr) => acc + curr);
+		console.log(sumArray);
+		sumArray < 8000 ? resolve(sumArray) : reject()
+	});
 
-    (sum < 8000) ? resolve(sum) : reject();
-  });
-
-  myPromise
-	.then(sum => console.log([2, 3, 5, 10].map(number => sum / number)))
-	.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+	promise
+		.then(sumArray => console.log([2, 3, 5, 10].map(number => sumArray / number)))
+		.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
 };
-
-fetchPromise();
+fetchPromise()
 
 // 5. Quando a Promise for bem-sucedida, encadeie nela uma segunda Promise que some os elementos do array.
 const fetchPromise = () => {
