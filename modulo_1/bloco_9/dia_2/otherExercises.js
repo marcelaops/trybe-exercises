232f89000a9c8a1db2723879411c1e5dd8e9820d
Marcela Silva <marcela.ops@hotmail.br>
2021-06-08 19:15:39 -0300

    exercise 3 final

--- modulo_1/bloco_9/dia_2/otherExercises.js
+++ modulo_1/bloco_9/dia_2/otherExercises.js
@@ -22,7 +22,7 @@ promise
 
 // 3. Quando a promise for resolvida com sucesso, retorne um array com 4 itens, sendo eles o resultado da divisão do numero resultante por 2, 3, 5 e 10.
 
-// Eu tinha feito da diferente, mas depois de ver os resursos adicionais e o gabarito percebi que era melhor colocar a promisse dentro de uma outra função para conseguir, sendo que a array da suma dividivo pela array [2, 3, 5, 10] ainda está saindo [ NaN, NaN, NaN, NaN ]
+// Eu tinha feito diferente, mas depois de ver os resursos adicionais e o gabarito percebi que era melhor colocar a promisse dentro de uma outra função, sendo que a array da sum dividivo pela array [2, 3, 5, 10] ainda está saindo [ NaN, NaN, NaN, NaN ] -> concertei na monitoria, eu estava esquecendo de retornar a sumArray na condição se caso desse resolve. -> resolve(sumArray)
 const fetchPromise = () => {
 	const promise = new Promise((resolve, reject) => {
 		const arrayNumber = [];
@@ -34,38 +34,17 @@ const fetchPromise = () => {
 		console.log(square);
 		const sumArray = square.reduce((acc, curr) => acc + curr);
 		console.log(sumArray);
-		if (sumArray > 8000) {
-				return reject();
-		}
-		resolve();
+		sumArray < 8000 ? resolve(sumArray) : reject()
 	});
 
 	promise
 		.then(sumArray => console.log([2, 3, 5, 10].map(number => sumArray / number)))
 		.catch(() => console.log('Promise rejeitada'));
 };
-fetchPromise();
-
-// No gabarito:
-const fetchPromise = () => {
-  const myPromise = new Promise((resolve, reject) => {
-    const myArray = Array.from(
-      { length: 10 },
-      () =>  Math.floor(Math.random() * 50) + 1
-    );
-    const sum = myArray.map(number => number * number)
-                       .reduce((number, acc) => number + acc, 0);
-
-    (sum < 8000) ? resolve(sum) : reject();
-  });
-
-  myPromise
-    .then(sum => console.log([2, 3, 5, 10].map(number => sum / number)))
-    .catch(() => console.log('Promise rejeitada'));
-	
-};
-
-fetchPromise();
+fetchPromise()
+	// .then(sumArray => console.log([2, 3, 5, 10].map(number => sumArray / number)))
+	// .catch(() => console.log('Promise rejeitada'));
+	// Pode ser colocado o then e a catch na hora de chamar a func como a linha 41 e 42. Assim teria q dar um return promise no final da func fetchPromise. 
 
 // 4. Quando a Promise for rejeitada, imprima, via console.log , a frase "É mais de oito mil! Essa promise deve estar quebrada!"
 const fetchPromise = () => {
