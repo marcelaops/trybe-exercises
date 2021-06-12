eeb20ee321249bdce06296ed4095b2bb0f88b628
Marcela Silva <marcela.ops@hotmail.br>
2021-06-12 17:05:44 -0300

    organizando arquivos jest e package.json

---
+++ modulo_1/bloco_10/dia_1/myRemove.test.js
@@ -0,0 +1,6 @@
+// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+// implemente seus testes aqui
---
+++ modulo_1/bloco_10/dia_1/myRemoveWithoutCopy.test.js
@@ -0,0 +1,5 @@
+// A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
