6d68bcdcd9c0a31e1bfbd64b7dd7a4c3e8120f0b
Marcela Silva <marcela.ops@hotmail.br>
2021-06-13 11:35:25 -0300

    Parte I organizando arquivos

--- modulo_1/bloco_10/dia_1/myRemove.test.js
+++ modulo_1/bloco_10/dia_1/myRemove.test.js
@@ -1,11 +1,24 @@
 // Parte I Exercício 2
 // A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
-// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-// Verifique se o array passado por parâmetro não sofreu alterações
-// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+// 1.Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// 2.Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// 3.Verifique se o array passado por parâmetro não sofreu alterações
+// 4.Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 // implemente seus testes aqui
 
 const myRemove = require('./myRemove');
 
-    
+describe('Parte I Exercício 2 - função myRemove(arr, item)', () => {
+    it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+        expect()
+    // });
+    // it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    //     expect()
+    // });
+    // it('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+    //     expect()
+    // });
+    // it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    //     expect()
+    // });
+});    
