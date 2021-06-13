9730b1128d18d65cac8ba3aa3c4329810a6561d4
Marcela Silva <marcela.ops@hotmail.br>
2021-06-13 12:14:33 -0300

    Parte I Exercício 3 - Testes para função myRemoveWithoutCopy

--- modulo_1/bloco_10/dia_1/myRemoveWithoutCopy.js
+++ modulo_1/bloco_10/dia_1/myRemoveWithoutCopy.js
@@ -11,3 +11,5 @@ function myRemoveWithoutCopy(arr, item) {
 
   return arr;
 }
+console.log(myRemoveWithoutCopy([1, 2, 3, 4], 3));
+module.exports = myRemoveWithoutCopy;
--- modulo_1/bloco_10/dia_1/myRemoveWithoutCopy.test.js
+++ modulo_1/bloco_10/dia_1/myRemoveWithoutCopy.test.js
@@ -1,5 +1,27 @@
 // A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
-// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
-// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-// Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
-// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+// 1.Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+// 2.Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// 3.Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+// 4.Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+
+const myRemoveWithoutCopy = require('./myRemoveWithoutCopy');
+
+describe('Parte I Exercício 3 - função myRemoveWithoutCopy', () => {
+    it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+        expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([ 1, 2, 4 ]);
+    });
+
+    it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+        expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    });
+
+    it('Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações', () => {
+        expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([ 1, 2, 4 ]);
+    });
+
+    it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+        expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
+});
+
+// no final das contas esse foi exatamente igual ao exercício anterior da função myRemove.
