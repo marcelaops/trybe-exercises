e341a6f7cadc399e42f7d7502bf3bb0cb1bc4e10
Marcela Silva <marcela.ops@hotmail.br>
2021-06-14 21:13:32 -0300

    exercise 4

--- modulo_1/bloco_10/dia_2/exercise4.test.js
+++ modulo_1/bloco_10/dia_2/exercise4.test.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const { expect } = require("@jest/globals");
+// const { expect } = require("@jest/globals");
 
 const getRepos = (url) => {
   return fetch(url)
@@ -12,9 +12,13 @@ const getRepos = (url) => {
 // 4 - O código acima busca no GitHub de um usuário, de acordo com a URL, seus repositórios, e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram nessa lista.
 
 test('Test para ver se o repositórios se encontram na lista', () => {
-    expect.assertions(1);
     const url = 'https://api.github.com/orgs/tryber/repos'
+    // expect.assertions(2);
     return getRepos(url).then(repo => {
+        console.log(repo);
         expect(repo).toContain('sd-01-week4-5-project-todo-list');
+        expect(repo).toContain('sd-01-week4-5-project-meme-generator');
     });
 });
+
+// era p estar certo, por causa da similariedade com o gabarito, mas falando que fetch is not definned. :|
