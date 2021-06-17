24ae8cc7c318c588668d0f82cf53c3dfb82b844c
Marcela Silva <marcela.ops@hotmail.br>
2021-06-17 07:57:44 -0300

    exercício 3

--- modulo_1/bloco_10/dia_3/random.mock.test.js
+++ modulo_1/bloco_10/dia_3/random.mock.test.js
@@ -24,7 +24,7 @@ test('testar o mock que faz uma nova implementação, onde recebe dois parâmetr
 })
 
 // 3.Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
-test('testar o mock que faz uma nova implementação, onde receba três parâmetros e retorne sua multiplicação. ', () => {
+test('testar o mock que faz uma nova implementação, onde receba três parâmetros e retorne seu dobro. ', () => {
 	const spyRandom = jest.spyOn(random, 'randomNumber');
 
 	spyRandom.mockImplementation((a, b, c) => a * b * c);
@@ -32,7 +32,7 @@ test('testar o mock que faz uma nova implementação, onde receba três parâmet
 	expect(spyRandom).toHaveBeenCalled();
 	expect(spyRandom).toHaveBeenCalledTimes(3); // aqui botei 3 pelo motivo do aí de cima. 
 	expect(spyRandom).toHaveBeenCalledWith(4, 2, 2);
-
+ 
 	spyRandom.mockReset();
 	expect(spyRandom).toHaveBeenCalledTimes(0);
 
@@ -43,3 +43,31 @@ test('testar o mock que faz uma nova implementação, onde receba três parâmet
 	expect(random.randomNumber).toHaveBeenCalledTimes(1);
 	expect(random.randomNumber).toHaveBeenCalledWith(4);
 })
+
+// No gabarito está diferente, ele faz um test para cada, dividindo:
+
+// const service = require('./service');
+
+// describe("testando implementações", () => {
+//   test("mockando função para receber 3 parâmetros e retornar sua multiplicação", () => {
+//     service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+//     expect(service.randomNumber(2, 3, 4)).toBe(24);
+//     expect(service.randomNumber).toHaveBeenCalled();
+//     expect(service.randomNumber).toHaveBeenCalledTimes(1);
+//     expect(service.randomNumber).toHaveBeenCalledWith(2, 3, 4);
+//   });
+
+//   test("mockando função que recebe um parâmetro e retorna seu dobro", () => {
+//     service.randomNumber.mockReset();
+//     expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+//     service.randomNumber.mockImplementation(a => a * 2);
+
+//     expect(service.randomNumber(5)).toBe(10);
+//     expect(service.randomNumber).toHaveBeenCalled();
+//     expect(service.randomNumber).toHaveBeenCalledTimes(1);
+//     expect(service.randomNumber).toHaveBeenCalledWith(5);
+//   });
+// });
+
