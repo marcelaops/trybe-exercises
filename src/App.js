bf24a20c86678fcd8409d3f385f3e667e0cb2a04
Marcela Silva <marcela.ops@hotmail.br>
2021-07-05 18:05:42 -0300

    utilizando o componente Switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -18,7 +18,17 @@ class App extends Component {
 
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
-        <Route exact path="/users" render={() =>  <Users greetingsMessage="Good Morning" />} />
+        <Route exact path="/users" component={Users}/>
+
+        <Switch>
+            <Route exact path="/" component={ Home }/>
+            <Route exact path="/about" component={ About }/>
+            <Route
+              exact
+              path="/users/:id"
+              render={(props) => <Users {...props} greetingMessage="Good Morning" /> }
+            />
+          </Switch>
       </BrowserRouter>
     );
   }
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    const { greetingsMessage } = this.props
+    const { 
+      greetingsMessage = "Good Morning",
+      match: { params: { id }}
+    } = this.props
     return (
       <div>
         <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> {`${greetingsMessage}, My awesome Users ${id}`} </p>
       </div>
     );
   }
