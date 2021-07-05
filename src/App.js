b33dd149fd1779031d5ddf48f1d0816e48e56655
Marcela Silva <marcela.ops@hotmail.br>
2021-07-05 17:36:31 -0300

    passando uma prop p o componente Users no App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
-        <Route exact path="/users" component={Users}/>
+        <Route exact path="/users" render={() =>  <Users greetingsMessage="Good Morning" />} />
       </BrowserRouter>
     );
   }
