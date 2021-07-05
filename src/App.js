471fbc02c7d12d25f631ca967c34fcda5857dc5d
Marcela Silva <marcela.ops@hotmail.br>
2021-07-05 16:39:11 -0300

    substituindo o componente Home por um Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route path="/" component={Home}/>
+      </BrowserRouter>
     );
   }
 }
