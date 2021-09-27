f7520446adcaab21bb1ce48143c120e8dff77b7f
Marcela Silva <marcela.ops@hotmail.br>
2021-09-27 12:11:10 -0300

    INDEX challenges

--- back-end/bloco_22/dia_3/INDEX-challenges.sql
+++ back-end/bloco_22/dia_3/INDEX-challenges.sql
@@ -8,11 +8,24 @@ FROM sakila.category
 WHERE MATCH(name) AGAINST('action');
 
 -- Após ter excluído o índice, mensure o custo com a seguinte query:
+DROP INDEX index_category ON category;
+
 SELECT *
 FROM sakila.category
 WHERE name LIKE '%action%';
 
-
 -- 2. Verifique o impacto de um INDEX na tabela address (banco de dados sakila ) adicionando-o na coluna postal_code . Após ter adicionado o índice, mensure o custo da query utilizando o execution plan, como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo
 
+USE sakila;
+CREATE INDEX index_postal_code ON address(postal_code);
 
+-- Mensure o custo com a seguinte query:
+SELECT *
+FROM sakila.address
+WHERE postal_code = '36693';
+
+DROP INDEX index_postal_code ON address;
+
+SELECT *
+FROM sakila.address
+WHERE postal_code = '36693';
