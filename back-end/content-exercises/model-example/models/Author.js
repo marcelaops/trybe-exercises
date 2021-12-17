ccb0dbc6dfbf3e3ca22660f8f691414cd0c43584
Marcela Silva <marcela.ops@hotmail.com>
2021-12-17 13:37:25 -0300

    Content Exercises Model com MongoDB

--- back-end/content-exercises/model-example/models/Author.js
+++ back-end/content-exercises/model-example/models/Author.js
@@ -1,4 +1,5 @@
 const connection = require('./connection');
+const { ObjectId } = require('mongodb');
 
 // Cria uma string com o nome completo do autor
 
@@ -27,38 +28,71 @@ const serialize = (authorData) => ({
   lastName: authorData.last_name
 });
 
-// Busca todos os autores do banco.
+// Busca todos os autores do banco com o MySQL.
 
+// const getAll = async () => {
+//   const [authors] = await connection.execute(
+//     'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
+//   );
+//   return authors.map(serialize).map(getNewAuthor);
+// };
+
+// Busca todos os autores do banco com o MongoDB.
 const getAll = async () => {
-  const [authors] = await connection.execute(
-    'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
-  );
-  return authors.map(serialize).map(getNewAuthor);
-};
+  return connection()
+      .then((db) => db.collection('authors').find().toArray())
+          .then((authors) =>
+              authors.map(({ _id, firstName, middleName, lastName }) =>
+              getNewAuthor({
+                  id: _id,
+                  firstName,
+                  middleName,
+                  lastName,
+              })
+          )
+      );
+}
 
 /*
 Busca um autor específico, a partir do seu ID
 @param {String} id ID do autor a ser recuperado
 */
+// MySQL
 
+// const findById = async (id) => {
+//   // Repare que substituímos o id por `?` na query.
+//   // Depois, ao executá-la, informamos um array com o id para o método `execute`.
+//   // O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
+//   const query = 'SELECT id, first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
+//   const [ authorData ] = await connection.execute(query, [id]);
+  
+//   if (authorData.length === 0) return null;
+  
+//   // Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
+//   const { firstName, middleName, lastName } = serialize(authorData[0]);
+  
+//   return getNewAuthor({
+//     id,
+//     firstName,
+//     middleName,
+//     lastName
+//   });
+// };
+
+// Mongo
 const findById = async (id) => {
-  // Repare que substituímos o id por `?` na query.
-  // Depois, ao executá-la, informamos um array com o id para o método `execute`.
-  // O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
-  const query = 'SELECT id, first_name, middle_name, last_name FROM model_example.authors WHERE id = ?'
-  const [ authorData ] = await connection.execute(query, [id]);
-  
-  if (authorData.length === 0) return null;
-  
-  // Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
-  const { firstName, middleName, lastName } = serialize(authorData[0]);
-  
-  return getNewAuthor({
-    id,
-    firstName,
-    middleName,
-    lastName
-  });
+  if (!ObjectId.isValid(id)) {
+      return null;
+  }
+
+  const authorData = await connection()
+      .then((db) => db.collection('authors').findOne(new ObjectId(id)));
+
+  if (!authorData) return null;
+
+  const { firstName, middleName, lastName } = authorData;
+
+  return getNewAuthor({ id, firstName, middleName, lastName });
 };
 
 // Criando um novo escritor
@@ -71,10 +105,18 @@ const isValid = (firstName, middleName, lastName) => {
   return true;
 };
 
-const create = async (firstName, middleName, lastName) => connection.execute(
-  'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
-  [firstName, middleName, lastName],
-);
+// func create com MySQL
+// const create = async (firstName, middleName, lastName) => connection.execute(
+//   'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?,?,?)',
+//   [firstName, middleName, lastName],
+// );
+
+// func create com Mongo
+const create = async (firstName, middleName, lastName) =>
+    connection()
+        .then((db) => db.collection('authors').insertOne({ firstName, middleName, lastName }))
+        .then(result => getNewAuthor({ id: result.insertedId, firstName, middleName, lastName }));
+  
 
 module.exports = {
    getAll,
--- back-end/content-exercises/model-example/models/Book.js
+++ back-end/content-exercises/model-example/models/Book.js
@@ -1,71 +1,96 @@
 const connection = require('./connection');
-const Author = require('./models/Author');
+// const Author = require('./Author');
+const { ObjectId } = require('mongodb');
 
 
-// Crie um modelo Book e defina o método getAll para retornar a lista de todos os livros.
+// Crie um modelo Book e defina o método getAll para retornar a lista de todos os livros. Com o MYSQL.
 
+// const getAll = async () => {
+//   const [books] = await connection.execute(
+//     'SELECT id, title, author_id FROM model_example.books',
+//   );
+
+//   return books.map(({ id, title, author_id }) => ({
+//     id,
+//     title,
+//     authorId: author_id,
+//   }));
+// }
+
+// Refatore o método getAll de models/Book para utilizar o mongo como banco de dados.
 const getAll = async () => {
-  const [books] = await connection.execute(
-    'SELECT id, title, author_id FROM model_example.books',
-  );
-
-  return books.map(({ id, title, author_id }) => ({
-    id,
-    title,
-    authorId: author_id,
-  }));
+  return connection()
+    .then((db) => db.collection('books').find().toArray());    
 }
 
 // Crie um método getByAuthorId no modelo Book , para retornar apenas livros associados com um determinado author_id . E altere o middleware da rota books criado no passo 2 para receber uma query string com a chave author_id , e retornar apenas os livros associados.
 
-const getByAuthorId = async (authorId) => {
-  const query = 'SELECT * FROM model_example.books WHERE author_id=?';
+// const getByAuthorId = async (authorId) => {
+//   const query = 'SELECT * FROM model_example.books WHERE author_id=?';
 
-  const [books] = await connection.execute(query, [authorId]);
+//   const [books] = await connection.execute(query, [authorId]);
 
-  return books.map(({ id, title, author_id }) => ({
-    id,
-    title,
-    authorId: author_id,
-  }));
+//   return books.map(({ id, title, author_id }) => ({
+//     id,
+//     title,
+//     authorId: author_id,
+//   }));
  
+// } 
+
+// Refatore o método getAll de models/Book para utilizar o mongo como banco de dados.
+
+const getByAuthorId = async (authorId) => {
+  connection()
+    .then((db) => db.collection('books').find({ author_id: Number(authorId) }).toArray());
 } 
 
-// Crie uma rota /books/:id e retorne o livro de acordo com o id passado por parâmetro. Se não existir, retorne um json no seguinte formato { message: 'Not found' } .
 
+// Crie uma rota /books/:id e retorne o livro de acordo com o id passado por parâmetro. Se não existir, retorne um json no seguinte formato { message: 'Not found' } .
+// MySQL
+// const getById = async (id) => {
+//   const query = 'SELECT * FROM model_example.books WHERE id = ?'
+//   const [ book ] = await connection.execute(query, [id]);
+  
+//   if (book.length === 0) return null;
+  
+//   return book.map(({ id, title, author_id }) => ({
+//     id,
+//     title,
+//     authorId: author_id,
+//   }));
+// }
+
+// Mongo - Refatore o método getById de models/Book para utilizar o mongo como banco de dados.
 const getById = async (id) => {
-  const query = 'SELECT * FROM model_example.books WHERE id = ?'
-  const [ book ] = await connection.execute(query, [id]);
-  
-  if (book.length === 0) return null;
-  
-  return book.map(({ id, title, author_id }) => ({
-    id,
-    title,
-    authorId: author_id,
-  }));
+  const book = await connection()
+    .then((db) => db.collection('books').findOne(new ObjectId(id)));
+
+  if (!book) return null;
+
+  return book;
 }
 
 // Na tabela books crie uma rota books do tipo POST . Faça as seguintes validações: Título não pode ser vazio; Título precisa ter pelo menos três caracteres; O campo author_id não pode ser vazio; O campo author_id só é válido se existir um autor com esse id;
 
 // Se algum dos requisitos anteriores não for atendido, retornar um json no seguinte formato { message: 'Dados inválidos' } com status 400 . Caso contrário, insira o livro na tabela books e retorne o json { message: 'Livro criado com sucesso! '} com o status 201 .
 
-const bookIsValid = () => {
-  if (!title || typeof title !== 'string' || title.length < 3) return false;
-  if (!authorId || typeof authorId !== 'number' || !(await Author.findById(authorId))) return false;
+// const bookIsValid = () => {
+//   if (!title || typeof title !== 'string' || title.length < 3) return false;
+//   if (!authorId || typeof authorId !== 'number' || !(await Author.findById(authorId))) return false;
 
-  return true;
-}
+//   return true;
+// }
 
-const bookCreate = () => connection.execute(
-  'INSERT INTO model_example.books (title, author_id) VALUES (?, ?)',
-  [title, authorId],
-);
+// const bookCreate = () => connection.execute(
+//   'INSERT INTO model_example.books (title, author_id) VALUES (?, ?)',
+//   [title, authorId],
+// );
 
 module.exports = {
   getAll,
   getByAuthorId,
   getById,
-  bookIsValid,
-  createBook,
+  // bookIsValid,
+  // bookCreate,
 };
