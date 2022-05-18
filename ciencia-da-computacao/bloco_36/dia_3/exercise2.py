3949f5935cd90b7706942862d51078057c7bdecd
Marcela Silva <marcela.ops@hotmail.com>
2022-05-18 18:20:30 -0300

    36.3 exercises

---
+++ ciencia-da-computacao/bloco_36/dia_3/exercise2.py
@@ -0,0 +1,10 @@
+""" Exercício 2 Demonstre o passo a passo, do processo de mistura, de um array sendo ordenado, utilizando merge sort . Comece o passo a passo a partir da linha abaixo:
+    7 3    5 4    6 8    2 1 """
+
+# 1º Separamos em porções menores: 7  3  5  4  6  8  2  1
+
+# 2º Reagrupando: 37 45 68 12
+
+# 3º Continua reagrupando: 3457 1268
+
+# 4º Por fim misturamos tudo: 12345678
